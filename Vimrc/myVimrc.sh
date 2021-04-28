e26df1c05b83fbfffca723855f44a98f107f17ff
Koki Satani <n20010@std.it-college.ac.jp>
2021-04-28 05:37:10 +0000

    made installer & fixed file path

--- Vimrc/myVimrc.sh
+++ Vimrc/myVimrc.sh
@@ -8,10 +8,10 @@ if [ ! -e $_VIMRC ]; then
     touch $_VIMRC
 fi
 
-cp sources/vimrc $_VIMRC
+cp Vimrc/sources/vimrc $_VIMRC
 
 if [ ! -d $_TEMPLATE ]; then
     mkdir -p $_TEMPLATE
 fi
 
-cp -a  --force sources/templates/* $_TEMPLATE
+cp -a  --force Vimrc/sources/templates/* $_TEMPLATE
