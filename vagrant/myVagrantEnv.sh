134e7b1eacfe00f905b46ed0b0503ecd986de7f5
KokiSatani <n20010@std.it-college.ac.jp>
2021-04-30 20:20:24 +0900

    add refresh command to installer

--- vagrant/myVagrantEnv.sh
+++ vagrant/myVagrantEnv.sh
@@ -5,14 +5,11 @@ _VAGRANT_DIR="$HOME/vagrantBoxes"
 vagrant -v &> /dev/null
 if [ $? -ne 0 ]; then
     sudo apt install vagrant
-    echo 'install vagrant'
 else
-    echo 'vagrant is exits'
+    echo 'Vagrant command has already exists.'
 fi
 
 if [ ! -d $_VAGRANT_DIR ]; then
     mkdir -p $_VAGRANT_DIR
-    echo 'mkdir'
-else
-    echo 'dir is exits'
 fi
+
