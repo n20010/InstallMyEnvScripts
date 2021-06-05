c9d2298fb145c365d8bcd2f20b86d9fc57bbd666
Koki Satani <n20010@std.it-college.ac.jp>
2021-06-05 14:29:58 +0900

    Fixed: file path in neovim installer

--- neovim/install_neovim.sh
+++ neovim/install_neovim.sh
@@ -14,4 +14,4 @@ sudo apt install curl
 curl -fLo "$_PLUGIN_DIR/plug.vim" --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-source ./neovim/python3.conf.sh
+source ./python3.conf.sh
